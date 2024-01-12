d7153547a97f023a606e76c0ef7509808af1b5a5
Shih-Wen WU (Peter) <shihwenwutw@gmail.com>
2024-01-12 13:50:33 +0800

    create template

--- handler.py
+++ handler.py
@@ -5,7 +5,3 @@ def get_tdx_road_id(event, context):
     event['get_tdx_road_id'] = get_road_id()
     return event
 
-
-def get_tdx_link_id(event, context):
-    event['get_tdx_link_id'] = get_link_id(city_list=['高雄市'])
-    return event
