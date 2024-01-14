616d7a445f429b64ccbf72623938f695ada53b60
Shih-Wen WU (Peter) <shihwenwutw@gmail.com>
2024-01-14 21:22:49 +0800

    ver1

--- handler.py
+++ handler.py
@@ -1,7 +1,54 @@
-from helper import get_road_id, get_link_id
+import datetime
+import os
+import boto3
+from helper import get_tdx_result, json_generator
 
 
-def get_tdx_road_id(event, context):
-    event['get_tdx_road_id'] = get_road_id()
+def get_tdx_bus_route(event, context):
+    city_key = [#"Taipei"
+                #"NewTaipei"
+                "Taoyuan",
+                "Taichung",
+                "Tainan",
+                "Kaohsiung",
+                "Keelung",
+                "KinmenCounty",           
+                "Hsinchu",
+                "HsinchuCounty",          
+                "MiaoliCounty",
+                "NantouCounty",
+                "ChanghuaCounty",
+                "YunlinCounty",
+                "ChiayiCounty",
+                "Chiayi",
+                "PingtungCounty",
+                "HualienCounty",
+                "TaitungCounty",
+                "YilanCounty",
+                "PenghuCounty"
+                ]
+    result = {}
+    for city in city_key:
+        print(f'processing {city}...')
+        request_url = "https://tdx.transportdata.tw/api/basic/v2/Bus/Shape/City/"\
+                        f"{city}"\
+                        "?%24top=9999"\
+                        "&%24format=JSON"
+
+
+        data = get_tdx_result(url=request_url)
+        json_obj = json_generator(tdx_data=data, city=city)
+        ingest_time = datetime.datetime.now().strftime("%Y%m%d")
+        s3_client = boto3.client('s3')
+
+        s3_client.put_object(Body=json_obj,
+                            Bucket=os.environ['s3_bucket'],
+                            Key=f"line-string/tdx_bus_route_bq_format__{city}_{ingest_time}.json")
+        
+        result[f"routes_cnt_{city}"] = str(len(data)) 
+    event['result'] = result
+
+
     return event
+
 
--- helper.py
+++ helper.py
@@ -1,11 +1,29 @@
 import json
-import boto3
 import os
+import re
 import requests
 import datetime
-import time
-import pandas as pd
-import awswrangler as wr
+
+
+# Json helper class
+def flatten_json(y):
+    out = {}
+
+    def flatten(x, name=''):
+        if type(x) is dict:
+            for a in x:
+                flatten(x[a], name + a + '_')
+        elif type(x) is list:
+            i = 0
+            for a in x:
+                flatten(a, name + str(i) + '_')
+                i += 1
+        else:
+            out[name[:-1]] = x
+
+    flatten(y)
+    return out
+
 
 # TDX helper class
 class Auth():
@@ -43,8 +61,8 @@ class Data():
             'authorization': 'Bearer '+access_token
         }
 
-def get_tdx_result(app_id=os.environ['tdx_client_id_1'], 
-                   app_key=os.environ['tdx_client_secret_1'], 
+def get_tdx_result(app_id=os.environ['tdx_client_id'], 
+                   app_key=os.environ['tdx_client_secret'], 
                    auth_url=os.environ['tdx_auth_url'],
                    url=None):
   a = Auth(app_id, app_key)
@@ -55,21 +73,87 @@ def get_tdx_result(app_id=os.environ['tdx_client_id_1'],
   return json.loads(data_response.text)
 
 
-def get_existing_file(data_type:str)->list:
+
+def geometry_generator(feature_string:str, city:str)->dict:
     '''
-    Args: 
-        --
+    converting string location data into geojson format
+
+    Args:
+        feature_string: string returned by TDX api
     
     Returns:
-        A list of existing station and exit file
+        a dictionary in for value of geometry key for geojson file
     '''
-    s3 = boto3.resource('s3')
-    bucket = s3.Bucket('online-data-lake-thirty-three')
+    geo_type = re.match('[a-zA-z]+',feature_string).group(0)
+    result_coordinates = []
+    #print(f"geo_type:{geo_type}")
+    if geo_type == 'LINESTRING':
+        #print(f"LINESTRING")
+        start_idx = feature_string.find('(')
+        end_idx = feature_string.find(')')
+        if city in ["Taipei","NewTaipei"]:
+            loc_list = feature_string[start_idx+1:end_idx].split(', ')
+        else:
+            loc_list = feature_string[start_idx+1:end_idx].split(',')
+        # loc_list:['121.625813 25.059209', '121.627575 25.060679', '121.628098 25.061084', ...]
+        #print(f"loc_list:{loc_list}")
 
-    bucket_object = bucket.objects.filter(Prefix=f'{data_type}/tdx')
-    if bucket_object != []:
-        objects = [obj.key.split('/')[1] for obj in bucket_object]
-        return objects#'_'.join(objects)
-    return []
+        for loc in loc_list:
+            split_loc = loc.split(' ')
+            num_split_loc = list(map(float, split_loc))
+
+            result_coordinates.append(num_split_loc)
+
+    elif geo_type == 'MULTILINESTRING':
+        #print(f"{feature_string}")
+        start_idx = [idx for idx, s in enumerate(feature_string) if '(' in s][1:]
+        end_idx = [idx for idx, s in enumerate(feature_string) if ')' in s][:-1]
+
+        loc_list_group = []
+        for i in range(len(start_idx)):
+            if city in ["Taipei","NewTaipei"]:
+                loc_list = feature_string[start_idx[i]+1:end_idx[i]].split(', ')
+            else:
+                loc_list = feature_string[start_idx[i]+1:end_idx[i]].split(',')
+            loc_list_group.append(loc_list)
+            #display(loc_list_group)
+
+        for loc_list in loc_list_group:
+            #print(loc_list)
+            refined_log_list = []
+            for loc in loc_list:
+                split_loc = loc.split(' ')
+                num_split_loc = list(map(float, split_loc))
+                refined_log_list.append(num_split_loc)
+            result_coordinates.append(refined_log_list)
+
+    return {"type":geo_type, "coordinates":result_coordinates}
 
 
+def json_generator(tdx_data:list, city)->str:
+    '''
+    merge exising tdx data with geometry value generated from geometry_generator, 
+    and generate a newline-delimited json file in string
+
+    Args:
+        tdx_data: string returned by TDX api
+    
+    Returns:
+        a dictionary in for value of geometry key for geojson file
+    '''
+    lst = []
+    for route in tdx_data:
+        flat = flatten_json(route)
+        new_route_obj = {}
+        for k, v in flat.items():   
+            if k != 'Geometry':
+                new_route_obj[k] = v
+            else:
+                new_route_obj["geometry"] = json.dumps(geometry_generator(v, city))
+            
+            new_route_obj["ingest_time"] = datetime.datetime.now().strftime("%Y%m%d")
+            new_route_obj["city"] = city
+
+        lst.append(new_route_obj)
+
+    return '\n'.join(map(json.dumps, lst))
